f68779c1661ca9109780998a613b87e69902bec0
Dora <55266839@qq.com>
2025-11-12 23:26:07 +0800

    v0.0.002

--- src/renderer/src/components/tabbar/Tab.tsx
+++ src/renderer/src/components/tabbar/Tab.tsx
@@ -9,12 +9,9 @@ const Tab: React.FC<TabProps> = ({ active }): React.ReactElement => {
   return (
     <div className={cn('h-full toolbar-tab-button pt-[4px] box-border')}>
       <div
-        className={cn(
-          'h-[36px] pb-[6px]! flex items-center py-[2px] px-[4px] box-border rounded-t-lg relative toolbar-btn-button-active',
-          {
-            'bg-white': active
-          }
-        )}
+        className={cn('h-[36px] pb-[6px] flex items-center px-[4px] box-border rounded-t-lg relative', {
+          'bg-white toolbar-btn-button-active': active
+        })}
       >
         <div
           className={cn('flex h-[28px] items-center gap-[4px] px-[8px] py-[4px] rounded-[6px] box-border', {
